f3f60ff969f2c9cf4494b6e41080764fb53d5866
Warrior80 <68621329+Warrior80@users.noreply.github.com>
2022-07-17 23:27:00 -0300

    Example Module

---
+++ src/main/java/com/elysian/client/module/ModuleExample.java
@@ -0,0 +1,60 @@
+package com.elysian.client.module;
+
+import net.minecraft.tileentity.TileEntityStructure.Mode;
+import net.minecraftforge.client.event.RenderWorldLastEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+
+import com.elysian.client.event.events.InputEvent;
+import com.elysian.client.event.events.PacketEvent;
+import com.elysian.client.module.ToggleableModule;
+import com.elysian.client.property.EnumProperty;
+import com.elysian.client.property.NumberProperty;
+
+public class ModuleExample extends ToggleableModule {
+
+    private final NumberProperty<Integer> exampleInteger = new NumberProperty<Integer>(5, 0, 50, "Example");
+    private final NumberProperty<Double> exampleDouble = new NumberProperty<Double>(5.0D, 0.0D, 50.0D, "Speed");
+    private final NumberProperty<Float> exampleFloat = new NumberProperty<Float>(5.00f, 0.00f, 50.00f, "Example");
+    private final EnumProperty<Mode> exampleMode = new EnumProperty<Mode>(Mode.ONE, "Example", "m");
+
+    public enum Mode {
+        ONE,
+        TWO
+    }
+
+    public ModuleExample() {
+        super("ModuleExample", new String[] {"Example"}, "Example", ModuleType.MISC);
+        this.offerProperties(exampleInteger, exampleDouble, exampleFloat, exampleMode, this.keybind);
+    }
+    
+    @Override
+    protected void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    protected void onDisable() {
+        super.onDisable();
+    }
+
+    @Override
+    public void update(TickEvent event) {
+
+    }
+
+    @Override
+    public void packet(PacketEvent event) {
+        
+    }
+    
+    @Override
+    public void render(RenderWorldLastEvent event) {
+
+    }
+
+    @Override
+    public void input(InputEvent event) {
+
+    }
+
+}
