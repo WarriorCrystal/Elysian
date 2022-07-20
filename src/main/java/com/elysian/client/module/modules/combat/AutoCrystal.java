e8c0989d2193ab516269e3731c8734c1181e6098
Lyo <v3nuz123@gmail.com>
2022-07-20 14:00:20 -0500

    AutoCrystal WP3 JAJAJJAJ

--- src/main/java/com/elysian/client/module/modules/combat/AutoCrystal.java
+++ src/main/java/com/elysian/client/module/modules/combat/AutoCrystal.java
@@ -21,48 +21,48 @@ public class AutoCrystal extends ToggleableModule {
     private final NumberProperty<Integer> breakDelay  = new NumberProperty<Integer>(0, 0, 10, "Break Delay");
 
     //Damages
-    private final Property<Boolean> sortBlocks  = new Property<Boolean>(true, "Sort Blocks", "Sort Blocks");
-    private final Property<Boolean> ignoreSelfDamage  = new Property<Boolean>(true, "Ignore self damage", "Example");
+    private final Property<Boolean> sortBlocks  = new Property<Boolean>(false, "Sort Blocks", "Sort Blocks");
+    private final Property<Boolean> ignoreSelfDamage  = new Property<Boolean>(false, "Ignore self damage", "Example");
     private final NumberProperty<Integer> minPlace  = new NumberProperty<Integer>(5, 0, 50, "Min Place");
     private final NumberProperty<Integer> maxSelfPlace  = new NumberProperty<Integer>(5, 0, 50, "Max Self Place");
     private final NumberProperty<Integer> minBreak = new NumberProperty<Integer>(5, 0, 50, "Min Break");
     private final NumberProperty<Integer> maxSelfBreak  = new NumberProperty<Integer>(5, 0, 50, "Max Self Break");
-    private final Property<Boolean> antiSuicide  = new Property<Boolean>(true, "Anti Suicide", "Anti Suicide");
+    private final Property<Boolean> antiSuicide  = new Property<Boolean>(false, "Anti Suicide", "Anti Suicide");
 
     //general
-    private final EnumProperty<rotatemode> rotateMode  = new EnumProperty<rotatemode>(rotatemode.Off, "Rotate Mode", "rm");
+        private final EnumProperty<rotatemode> rotateMode  = new EnumProperty<rotatemode>(rotatemode.Off, "Rotate Mode", "rm");
     private final NumberProperty<Integer> maxYaw  = new NumberProperty<Integer>(180, 0, 180, "Max Yaw");
-    private final Property<Boolean> raytrace  = new Property<Boolean>(true, "RayTrace", "RayTrace");
+    private final Property<Boolean> raytrace  = new Property<Boolean>(false, "RayTrace", "RayTrace");
     private final EnumProperty<fastmode> fastMode  = new EnumProperty<fastmode>(fastmode.Off, "Fast Mode", "Fast Mode");
     private final EnumProperty<AutoSwitch> autoSwitch  = new EnumProperty<AutoSwitch>(AutoSwitch.Allways, "Auto Switch", "Auto Switch");
-    private final Property<Boolean> silentSwitchHand  = new Property<Boolean>(true, "Silent Switch", "Silent Switch");
-    private final Property<Boolean> antiWeakness  = new Property<Boolean>(true, "AntiWeakness", "AntiWeakness");
+    private final Property<Boolean> silentSwitchHand  = new Property<Boolean>(false, "Silent Switch", "Silent Switch");
+    private final Property<Boolean> antiWeakness  = new Property<Boolean>(false, "AntiWeakness", "AntiWeakness");
     private final NumberProperty<Integer> maxCrystals  = new NumberProperty<Integer>(1, 1, 4, "MaxCrystals");
-    private final Property<Boolean> ignoreTerrain  = new Property<Boolean>(true, "Ignore Terrain", "Ignore Terrain");
+    private final Property<Boolean> ignoreTerrain  = new Property<Boolean>(false, "Ignore Terrain", "Ignore Terrain");
     private final EnumProperty<crystallogic> crystalLogic  = new EnumProperty<crystallogic>(crystallogic.Damage, "Crystal Logic", "Crystal Logic");
-    private final Property<Boolean> thirteen  = new Property<Boolean>(true, "1.13", "13");
-    private final Property<Boolean> attackPacket  = new Property<Boolean>(true, "Attack Packet", "Attack Packet");
-    private final Property<Boolean> packetSafe  = new Property<Boolean>(true, "Packet Safe", "Packet Safe");
-    private final Property<Boolean> noBreakCalcs  = new Property<Boolean>(true, "No Break Calcs", "NOBREAKCALCS");
+    private final Property<Boolean> thirteen  = new Property<Boolean>(false, "1.13", "13");
+    private final Property<Boolean> attackPacket  = new Property<Boolean>(false, "Attack Packet", "Attack Packet");
+    private final Property<Boolean> packetSafe  = new Property<Boolean>(false, "Packet Safe", "Packet Safe");
+    private final Property<Boolean> noBreakCalcs  = new Property<Boolean>(false, "No Break Calcs", "NOBREAKCALCS");
     private final EnumProperty<ArrayListMode> arrayListMode  = new EnumProperty<ArrayListMode>(ArrayListMode.CPS, "Arraylist Mode", "alm");
-    private final Property<Boolean> debug  = new Property<Boolean>(true, "Debug", "Db");
+    private final Property<Boolean> debug  = new Property<Boolean>(false, "Debug", "Db");
 
     //misc
-    private final Property<Boolean> threaded  = new Property<Boolean>(true, "Threaded", "Threaded");
-    private final Property<Boolean> antiStuck  = new Property<Boolean>(true, "Anti Stuck", "Anti Stuck");
+    private final Property<Boolean> threaded  = new Property<Boolean>(false, "Threaded", "Threaded");
+    private final Property<Boolean> antiStuck  = new Property<Boolean>(false, "Anti Stuck", "Anti Stuck");
     private final NumberProperty<Integer> maxAntiStuckDamage  = new NumberProperty<Integer>(8, 0, 36, "Stuck Self Damage");
 
     //predict
-    private final Property<Boolean> predictCrystal  = new Property<Boolean>(true, "Predict Crystal", "Predict Crystal");
-    private final Property<Boolean> predictBlock  = new Property<Boolean>(true, "Predict Block", "Predict Block");
+    private final Property<Boolean> predictCrystal  = new Property<Boolean>(false, "Predict Crystal", "Predict Crystal");
+    private final Property<Boolean> predictBlock  = new Property<Boolean>(false, "Predict Block", "Predict Block");
     private final EnumProperty<ModuleExample.Mode> predictTeleport  = new EnumProperty<ModuleExample.Mode>(ModuleExample.Mode.ONE, "Predict Teleport", "Predict Teleport");
-    private final Property<Boolean> entityPredict  = new Property<Boolean>(true, "Entity Predict", "Entity Predict");
+    private final Property<Boolean> entityPredict  = new Property<Boolean>(false, "Entity Predict", "Entity Predict");
     private final NumberProperty<Integer> predictedTicks = new NumberProperty<Integer>(5, 0, 50, "Predicted Ticks");
 
     //Feet Obby stuff
-    private final Property<Boolean> palceObiFeet   = new Property<Boolean>(true, "Enabled", "Enabled");
-    private final Property<Boolean> ObiYCheck   = new Property<Boolean>(true, "YCheck", "YCheck");
-    private final Property<Boolean> rotateObiFeet  = new Property<Boolean>(true, "Rotate", "Rotate");
+    private final Property<Boolean> palceObiFeet   = new Property<Boolean>(false, "Enabled", "Enabled");
+    private final Property<Boolean> ObiYCheck   = new Property<Boolean>(false, "YCheck", "YCheck");
+    private final Property<Boolean> rotateObiFeet  = new Property<Boolean>(false, "Rotate", "Rotate");
     private final NumberProperty<Integer> timeoutTicksObiFeet  = new NumberProperty<Integer>(3, 0, 5, "Timeout");
 
     //Faceplace
@@ -72,20 +72,16 @@ public class AutoCrystal extends ToggleableModule {
     private final NumberProperty<Integer> fuckArmourHP  = new NumberProperty<Integer>(0, 0, 100, "Armour%");
 
     //render
-    private final EnumProperty<ModuleExample.Mode> when  = new EnumProperty<ModuleExample.Mode>(ModuleExample.Mode.ONE, "Example", "m");
-    private final EnumProperty<ModuleExample.Mode> mode  = new EnumProperty<ModuleExample.Mode>(ModuleExample.Mode.ONE, "Example", "m");
-    private final Property<Boolean> fade  = new Property<Boolean>(true, "Fade", "Fade");
-    private final NumberProperty<Integer> fadeTime  = new NumberProperty<Integer>(5, 0, 50, "Example");
-    private final Property<Boolean> flat  = new Property<Boolean>(true, "Flat", "flat");
-    private final NumberProperty<Float> height  = new NumberProperty<Float>(5.00f, 0.00f, 50.00f, "Height");
-    private final NumberProperty<Integer> width  = new NumberProperty<Integer>(5, 0, 50, "Width");
-    private final NumberProperty<Integer> renderFillColour  = new NumberProperty<Integer>(5, 0, 50, "Render Fill Colour");
-    private final NumberProperty<Integer> renderBoxColour  = new NumberProperty<Integer>(5, 0, 50, "Render Box Colour");
-    private final Property<Boolean> renderDamage  = new Property<Boolean>(true, "Render Damage", "Render Damage");
-    private final EnumProperty<ModuleExample.Mode> swing  = new EnumProperty<ModuleExample.Mode>(ModuleExample.Mode.ONE, "Swing", "Swing");
-    private final Property<Boolean> placeSwing = new Property<Boolean>(true, "Place Swing", "Place Swing");
-
-
+    private final EnumProperty<RenderW> when  = new EnumProperty<RenderW>(RenderW.Break, "Example", "m");
+    private final EnumProperty<RenderM> mode  = new EnumProperty<RenderM>(RenderM.Both, "Example", "m");
+    private final Property<Boolean> fade  = new Property<Boolean>(false, "Fade", "Fade");
+    private final NumberProperty<Integer> fadeTime  = new NumberProperty<Integer>(200, 0, 1000, "Example");
+    private final Property<Boolean> flat  = new Property<Boolean>(false, "Flat", "flat");
+    private final NumberProperty<Float> height  = new NumberProperty<Float>(0.2f, -2.0f, 2.0f, "Height");
+    private final NumberProperty<Integer> width  = new NumberProperty<Integer>(1, 1, 10, "Width");
+    private final Property<Boolean> renderDamage  = new Property<Boolean>(false, "Render Damage", "Render Damage");
+    private final EnumProperty<Swing> swing  = new EnumProperty<Swing>(Swing.Offhand, "Swing", "Swing");
+    private final Property<Boolean> placeSwing = new Property<Boolean>(false, "Place Swing", "Place Swing");
 
 
     public enum rotatemode {
@@ -142,6 +138,6 @@ public class AutoCrystal extends ToggleableModule {
     }
     public AutoCrystal() {
         super("AutoCrystal", new String[] {"AutoCrystal"}, "AutoCrystal", ModuleType.COMBAT);
-        this.offerProperties(keybind);
+        this.offerProperties(breakRange, placeRange, breakRangeWall, placeRangeWall, targetRange, placeDelay, breakDelay, sortBlocks, ignoreSelfDamage, minPlace, maxSelfPlace, minBreak, maxSelfBreak, antiSuicide, maxYaw, raytrace, fastMode, autoSwitch, silentSwitchHand, antiWeakness, maxCrystals, ignoreTerrain, crystalLogic, thirteen, attackPacket, packetSafe, noBreakCalcs, arrayListMode, debug, threaded, antiStuck, maxAntiStuckDamage, predictCrystal, predictBlock, predictTeleport, entityPredict, predictedTicks, palceObiFeet, ObiYCheck, rotateObiFeet, timeoutTicksObiFeet, noMP, facePlaceHP, facePlaceDelay, fuckArmourHP, when, mode, fade, fadeTime, flat, height, width, renderDamage, swing, placeSwing, keybind);
     }
 }
