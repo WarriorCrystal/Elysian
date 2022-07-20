4dfe7316e4712e613723f30d7ee9f5b99e3785ef
Warrior80 <68621329+Warrior80@users.noreply.github.com>
2022-07-20 11:46:27 -0300

    GreenChat

---
+++ src/main/java/com/elysian/client/module/modules/misc/GreenChat.java
@@ -0,0 +1,37 @@
+package com.elysian.client.module.modules.misc;
+
+
+import com.elysian.client.Elysian;
+import com.elysian.client.command.CommandManager;
+import com.elysian.client.event.events.PacketEvent;
+import com.elysian.client.module.ToggleableModule;
+import com.elysian.client.property.Property;
+
+import net.minecraft.network.play.client.CPacketChatMessage;
+
+import com.elysian.client.module.ModuleType;
+
+public class GreenChat extends ToggleableModule {
+
+    private final Property<Boolean> spaceToggle = new Property<Boolean>(true, "Space", "sp");
+
+    public GreenChat() {
+        super("GreenChat", new String[] {"ColorChat"}, "colors!!!!", ModuleType.MISC);
+        this.offerProperties(spaceToggle, this.keybind);
+    }
+
+    @Override
+    public void packet(PacketEvent event) {
+        if(event.getPacket() instanceof CPacketChatMessage){
+            if(((CPacketChatMessage) event.getPacket()).getMessage().startsWith("/") || ((CPacketChatMessage) event.getPacket()).getMessage().startsWith("-") || ((CPacketChatMessage) event.getPacket()).getMessage().startsWith("*") || ((CPacketChatMessage) event.getPacket()).getMessage().startsWith(".") || ((CPacketChatMessage) event.getPacket()).getMessage().startsWith("&") || ((CPacketChatMessage) event.getPacket()).getMessage().startsWith("%") || ((CPacketChatMessage) event.getPacket()).getMessage().startsWith("$") || ((CPacketChatMessage) event.getPacket()).getMessage().startsWith(",")) return;
+            String message = ((CPacketChatMessage) event.getPacket()).getMessage();
+            String space = "";
+            if(spaceToggle.getValue()) space = " "; 
+            if(!spaceToggle.getValue()) space = ""; 
+            String finalM = ">" + space + message;
+            if(finalM.length() > 255) return;
+            ((CPacketChatMessage) event.getPacket()).message = finalM;
+        }
+    }
+
+}
