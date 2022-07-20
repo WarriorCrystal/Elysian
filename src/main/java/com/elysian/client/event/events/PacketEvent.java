662b03bbfb6fee0b7c2fa3615b764c1b7e25a13a
Lyo <v3nuz123@gmail.com>
2022-07-20 11:20:35 -0500

    DOTGODAC

--- src/main/java/com/elysian/client/event/events/PacketEvent.java
+++ src/main/java/com/elysian/client/event/events/PacketEvent.java
@@ -2,35 +2,32 @@
 
 package com.elysian.client.event.events;
 
-import net.minecraftforge.fml.common.eventhandler.*;
-import net.minecraft.network.*;
 
-public class PacketEvent extends Event
-{
-    Packet packet;
-    
-    public boolean isCancelable() {
-        return true;
-    }
-    
-    public PacketEvent(final Packet packet) {
+import com.elysian.client.event.Event;
+import net.minecraft.network.Packet;
+
+public class PacketEvent extends Event {
+    private Packet packet;
+
+    public PacketEvent(Packet packet) {
         this.packet = packet;
     }
-    
+
     public Packet getPacket() {
         return this.packet;
     }
-    
-    public static class Receive extends PacketEvent
-    {
-        public Receive(final Packet packet) {
+
+    public void setPacket(Packet packet) {
+        this.packet = packet;
+    }
+
+    public static class Receive extends PacketEvent {
+        public Receive(Packet packet) {
             super(packet);
         }
     }
-    
-    public static class Send extends PacketEvent
-    {
-        public Send(final Packet packet) {
+    public static class Send extends PacketEvent {
+        public Send(Packet packet) {
             super(packet);
         }
     }
