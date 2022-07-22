6ca1a8fd6fbed1b4641cc6d27041acc9fc95f8f1
Venuz !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! <v3nuz123@gmail.com>
2022-07-21 19:55:57 -0500

    speedinstant

--- src/main/java/com/elysian/client/module/modules/movement/Speed.java
+++ src/main/java/com/elysian/client/module/modules/movement/Speed.java
@@ -1,19 +1,34 @@
 package com.elysian.client.module.modules.movement;
 
 import com.elysian.client.event.events.MoveEvent;
+import com.elysian.client.event.events.UpdateWalkingPlayerEvent;
+import com.elysian.client.module.ModuleExample;
 import com.elysian.client.module.ModuleType;
 import com.elysian.client.module.ToggleableModule;
+import com.elysian.client.property.EnumProperty;
+import com.elysian.client.property.NumberProperty;
 import com.elysian.client.property.Property;
 import com.elysian.client.util.EntityUtil;
+import net.minecraft.entity.Entity;
+import net.minecraft.init.MobEffects;
 import net.minecraft.util.MovementInput;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
+import java.util.Objects;
+import java.util.Random;
+
 public class Speed extends ToggleableModule {
 
+    private final EnumProperty<Mode> mode = new EnumProperty<Mode>(Mode.INSTANT, "Example", "m");
     private final Property<Boolean> strafeJump = new Property<Boolean>(true, "Strafe Jump", "Strafe Jump");
     private final Property<Boolean> noShake = new Property<Boolean>(true, "No Shake", "No Shake");
     private final Property<Boolean> useTimer = new Property<Boolean>(true, "Use Timer", "Use Timer");
+    private final NumberProperty<Double> zeroSpeed  = new NumberProperty<Double>(0.0D, 0.0D, 100.0D, "0 Speed");
+    private final NumberProperty<Double> speed  = new NumberProperty<Double>(10.0D, 0.1D, 100.0D, "Speed");
+    private final NumberProperty<Double> blocked  = new NumberProperty<Double>(10.0D, 0.0D, 100.0D, "Blocked");
+    private final NumberProperty<Double> unblocked  = new NumberProperty<Double>(10.0D, 0.0D, 100.0D, "Unblocked");
+
     private static Speed INSTANCE = new Speed();
     private double highChainVal = 0.0;
     private double lowChainVal = 0.0;
@@ -26,13 +41,405 @@ public class Speed extends ToggleableModule {
     public boolean changeY = false;
     private int vanillaCounter = 0;
 
+    public enum Mode {
+        INSTANT,
+        VANILLA
+    }
+
     public Speed() {
         super("SpeedInstant", new String[]{"SpeedInstant"}, "SpeedInstant", ModuleType.MOVEMENT);
-        this.offerProperties(strafeJump, noShake, useTimer, this.keybind);
+        this.offerProperties(strafeJump, noShake, useTimer, zeroSpeed, speed, blocked, unblocked, this.keybind);
+    }
+
+
+    private boolean shouldReturn() {
+        return false;
+    }
+
+    @Override
+    public void update(TickEvent event) {
+        if (this.shouldReturn() || Speed.mc.player.isSneaking() || Speed.mc.player.isInWater() || Speed.mc.player.isInLava()) {
+            return;
+        }
+
+        }
+
+    @SubscribeEvent
+    public void onUpdateWalkingPlayer(UpdateWalkingPlayerEvent event) {
+        if (this.mode.getValue() != Mode.VANILLA) {
+            return;
+        }
+    }
+
+    private double getJumpBoostModifier() {
+        double boost = 0.0;
+        if (Speed.mc.player.isPotionActive(MobEffects.JUMP_BOOST)) {
+            int amplifier = Objects.requireNonNull(Speed.mc.player.getActivePotionEffect(MobEffects.JUMP_BOOST)).getAmplifier();
+            boost *= 1.0 + 0.2 * (double)amplifier;
+        }
+        return boost;
+    }
+
+    private boolean vanillaCheck() {
+        if (Speed.mc.player.onGround) {
+            // empty if block
+        }
+        return false;
+    }
+
+    private boolean vanilla() {
+        return Speed.mc.player.onGround;
+    }
+
+    private void doBoost() {
+        this.bounceHeight = 0.4;
+        this.move = 0.26f;
+        if (Speed.mc.player.onGround) {
+            this.startY = Speed.mc.player.posY;
+        }
+        if (EntityUtil.getEntitySpeed((Entity)Speed.mc.player) <= 1.0) {
+            this.lowChainVal = 1.0;
+            this.highChainVal = 1.0;
+        }
+            this.oneTime = true;
+            this.antiShake = this.noShake.getValue() != false && Speed.mc.player.getRidingEntity() == null;
+            Random random = new Random();
+            boolean rnd = random.nextBoolean();
+            if (Speed.mc.player.posY >= this.startY + this.bounceHeight) {
+                Speed.mc.player.motionY = -this.bounceHeight;
+                this.lowChainVal += 1.0;
+                if (this.lowChainVal == 1.0) {
+                    this.move = 0.075f;
+                }
+                if (this.lowChainVal == 2.0) {
+                    this.move = 0.15f;
+                }
+                if (this.lowChainVal == 3.0) {
+                    this.move = 0.175f;
+                }
+                if (this.lowChainVal == 4.0) {
+                    this.move = 0.2f;
+                }
+                if (this.lowChainVal == 5.0) {
+                    this.move = 0.225f;
+                }
+                if (this.lowChainVal == 6.0) {
+                    this.move = 0.25f;
+                }
+                if (this.lowChainVal >= 7.0) {
+                    this.move = 0.27895f;
+                }
+            }
+            if (Speed.mc.player.posY == this.startY) {
+                Speed.mc.player.motionY = this.bounceHeight;
+                this.highChainVal += 1.0;
+                if (this.highChainVal == 1.0) {
+                    this.move = 0.075f;
+                }
+                if (this.highChainVal == 2.0) {
+                    this.move = 0.175f;
+                }
+                if (this.highChainVal == 3.0) {
+                    this.move = 0.325f;
+                }
+                if (this.highChainVal == 4.0) {
+                    this.move = 0.375f;
+                }
+                if (this.highChainVal == 5.0) {
+                    this.move = 0.4f;
+                }
+                if (this.highChainVal >= 6.0) {
+                    this.move = 0.43395f;
+                }
+
+            }
+            EntityUtil.moveEntityStrafe(this.move, (Entity)Speed.mc.player);
+            if (this.oneTime) {
+                Speed.mc.player.motionY = -0.1;
+                this.oneTime = false;
+            }
+            this.highChainVal = 0.0;
+            this.lowChainVal = 0.0;
+            this.antiShake = false;
+        }
+
+
+    private void doAccel() {
+        this.bounceHeight = 0.4;
+        this.move = 0.26f;
+        if (Speed.mc.player.onGround) {
+            this.startY = Speed.mc.player.posY;
+        }
+        if (EntityUtil.getEntitySpeed((Entity)Speed.mc.player) <= 1.0) {
+            this.lowChainVal = 1.0;
+            this.highChainVal = 1.0;
+        }
+            this.oneTime = true;
+            this.antiShake = this.noShake.getValue() != false && Speed.mc.player.getRidingEntity() == null;
+            Random random = new Random();
+            boolean rnd = random.nextBoolean();
+            if (Speed.mc.player.posY >= this.startY + this.bounceHeight) {
+                Speed.mc.player.motionY = -this.bounceHeight;
+                this.lowChainVal += 1.0;
+                if (this.lowChainVal == 1.0) {
+                    this.move = 0.075f;
+                }
+                if (this.lowChainVal == 2.0) {
+                    this.move = 0.175f;
+                }
+                if (this.lowChainVal == 3.0) {
+                    this.move = 0.275f;
+                }
+                if (this.lowChainVal == 4.0) {
+                    this.move = 0.35f;
+                }
+                if (this.lowChainVal == 5.0) {
+                    this.move = 0.375f;
+                }
+                if (this.lowChainVal == 6.0) {
+                    this.move = 0.4f;
+                }
+                if (this.lowChainVal == 7.0) {
+                    this.move = 0.425f;
+                }
+                if (this.lowChainVal == 8.0) {
+                    this.move = 0.45f;
+                }
+                if (this.lowChainVal == 9.0) {
+                    this.move = 0.475f;
+                }
+                if (this.lowChainVal == 10.0) {
+                    this.move = 0.5f;
+                }
+                if (this.lowChainVal == 11.0) {
+                    this.move = 0.5f;
+                }
+                if (this.lowChainVal == 12.0) {
+                    this.move = 0.525f;
+                }
+                if (this.lowChainVal == 13.0) {
+                    this.move = 0.525f;
+                }
+                if (this.lowChainVal == 14.0) {
+                    this.move = 0.535f;
+                }
+                if (this.lowChainVal == 15.0) {
+                    this.move = 0.535f;
+                }
+                if (this.lowChainVal == 16.0) {
+                    this.move = 0.545f;
+                }
+                if (this.lowChainVal >= 17.0) {
+                    this.move = 0.545f;
+                }
+            }
+            if (Speed.mc.player.posY == this.startY) {
+                Speed.mc.player.motionY = this.bounceHeight;
+                this.highChainVal += 1.0;
+                if (this.highChainVal == 1.0) {
+                    this.move = 0.075f;
+                }
+                if (this.highChainVal == 2.0) {
+                    this.move = 0.175f;
+                }
+                if (this.highChainVal == 3.0) {
+                    this.move = 0.375f;
+                }
+                if (this.highChainVal == 4.0) {
+                    this.move = 0.6f;
+                }
+                if (this.highChainVal == 5.0) {
+                    this.move = 0.775f;
+                }
+                if (this.highChainVal == 6.0) {
+                    this.move = 0.825f;
+                }
+                if (this.highChainVal == 7.0) {
+                    this.move = 0.875f;
+                }
+                if (this.highChainVal == 8.0) {
+                    this.move = 0.925f;
+                }
+                if (this.highChainVal == 9.0) {
+                    this.move = 0.975f;
+                }
+                if (this.highChainVal == 10.0) {
+                    this.move = 1.05f;
+                }
+                if (this.highChainVal == 11.0) {
+                    this.move = 1.1f;
+                }
+                if (this.highChainVal == 12.0) {
+                    this.move = 1.1f;
+                }
+                if (this.highChainVal == 13.0) {
+                    this.move = 1.15f;
+                }
+                if (this.highChainVal == 14.0) {
+                    this.move = 1.15f;
+                }
+                if (this.highChainVal == 15.0) {
+                    this.move = 1.175f;
+                }
+                if (this.highChainVal == 16.0) {
+                    this.move = 1.175f;
+                }
+                if (this.highChainVal >= 17.0) {
+                    this.move = 1.175f;
+                }
+            }
+            EntityUtil.moveEntityStrafe(this.move, (Entity)Speed.mc.player);
+            if (this.oneTime) {
+                Speed.mc.player.motionY = -0.1;
+                this.oneTime = false;
+            }
+            this.antiShake = false;
+            this.highChainVal = 0.0;
+            this.lowChainVal = 0.0;
+        }
+
+    private void doOnground() {
+        this.bounceHeight = 0.4;
+        this.move = 0.26f;
+        if (Speed.mc.player.onGround) {
+            this.startY = Speed.mc.player.posY;
+        }
+        if (EntityUtil.getEntitySpeed((Entity)Speed.mc.player) <= 1.0) {
+            this.lowChainVal = 1.0;
+            this.highChainVal = 1.0;
+        }
+        if (EntityUtil.isEntityMoving((Entity)Speed.mc.player) && !Speed.mc.player.collidedHorizontally) {
+            this.oneTime = true;
+            this.antiShake = this.noShake.getValue() != false && Speed.mc.player.getRidingEntity() == null;
+            Random random = new Random();
+            boolean rnd = random.nextBoolean();
+            if (Speed.mc.player.posY >= this.startY + this.bounceHeight) {
+                Speed.mc.player.motionY = -this.bounceHeight;
+                this.lowChainVal += 1.0;
+                if (this.lowChainVal == 1.0) {
+                    this.move = 0.075f;
+                }
+                if (this.lowChainVal == 2.0) {
+                    this.move = 0.175f;
+                }
+                if (this.lowChainVal == 3.0) {
+                    this.move = 0.275f;
+                }
+                if (this.lowChainVal == 4.0) {
+                    this.move = 0.35f;
+                }
+                if (this.lowChainVal == 5.0) {
+                    this.move = 0.375f;
+                }
+                if (this.lowChainVal == 6.0) {
+                    this.move = 0.4f;
+                }
+                if (this.lowChainVal == 7.0) {
+                    this.move = 0.425f;
+                }
+                if (this.lowChainVal == 8.0) {
+                    this.move = 0.45f;
+                }
+                if (this.lowChainVal == 9.0) {
+                    this.move = 0.475f;
+                }
+                if (this.lowChainVal == 10.0) {
+                    this.move = 0.5f;
+                }
+                if (this.lowChainVal == 11.0) {
+                    this.move = 0.5f;
+                }
+                if (this.lowChainVal == 12.0) {
+                    this.move = 0.525f;
+                }
+                if (this.lowChainVal == 13.0) {
+                    this.move = 0.525f;
+                }
+                if (this.lowChainVal == 14.0) {
+                    this.move = 0.535f;
+                }
+                if (this.lowChainVal == 15.0) {
+                    this.move = 0.535f;
+                }
+                if (this.lowChainVal == 16.0) {
+                    this.move = 0.545f;
+                }
+                if (this.lowChainVal >= 17.0) {
+                    this.move = 0.545f;
+                }
+            }
+            if (Speed.mc.player.posY == this.startY) {
+                Speed.mc.player.motionY = this.bounceHeight;
+                this.highChainVal += 1.0;
+                if (this.highChainVal == 1.0) {
+                    this.move = 0.075f;
+                }
+                if (this.highChainVal == 2.0) {
+                    this.move = 0.175f;
+                }
+                if (this.highChainVal == 3.0) {
+                    this.move = 0.375f;
+                }
+                if (this.highChainVal == 4.0) {
+                    this.move = 0.6f;
+                }
+                if (this.highChainVal == 5.0) {
+                    this.move = 0.775f;
+                }
+                if (this.highChainVal == 6.0) {
+                    this.move = 0.825f;
+                }
+                if (this.highChainVal == 7.0) {
+                    this.move = 0.875f;
+                }
+                if (this.highChainVal == 8.0) {
+                    this.move = 0.925f;
+                }
+                if (this.highChainVal == 9.0) {
+                    this.move = 0.975f;
+                }
+                if (this.highChainVal == 10.0) {
+                    this.move = 1.05f;
+                }
+                if (this.highChainVal == 11.0) {
+                    this.move = 1.1f;
+                }
+                if (this.highChainVal == 12.0) {
+                    this.move = 1.1f;
+                }
+                if (this.highChainVal == 13.0) {
+                    this.move = 1.15f;
+                }
+                if (this.highChainVal == 14.0) {
+                    this.move = 1.15f;
+                }
+                if (this.highChainVal == 15.0) {
+                    this.move = 1.175f;
+                }
+                if (this.highChainVal == 16.0) {
+                    this.move = 1.175f;
+                }
+                if (this.highChainVal >= 17.0) {
+                    this.move = 1.2f;
+                }
+                if (this.useTimer.getValue().booleanValue()) {
+                }
+            }
+            EntityUtil.moveEntityStrafe(this.move, (Entity)Speed.mc.player);
+        } else {
+            if (this.oneTime) {
+                Speed.mc.player.motionY = -0.1;
+                this.oneTime = false;
+            }
+            this.antiShake = false;
+            this.highChainVal = 0.0;
+            this.lowChainVal = 0.0;
+        }
     }
 
     @SubscribeEvent
-    public void onMove(MoveEvent event) {
+    public void onMode(MoveEvent event) {
+        if (!(this.shouldReturn() || event.getStage() != 0 || this.mode.getValue() != Mode.INSTANT || Speed.mc.player.isSneaking() || Speed.mc.player.isInWater() || Speed.mc.player.isInLava() || Speed.mc.player.movementInput.moveForward == 0.0f && Speed.mc.player.movementInput.moveStrafe == 0.0f)) {
             if (Speed.mc.player.onGround && this.strafeJump.getValue().booleanValue()) {
                 Speed.mc.player.motionY = 0.4;
                 event.setY(0.4);
@@ -60,5 +467,10 @@ public class Speed extends ToggleableModule {
             }
         }
     }
+
+
+    }
+
+
 
 
--- src/main/java/com/elysian/client/util/EntityUtil.java
+++ src/main/java/com/elysian/client/util/EntityUtil.java
@@ -8,6 +8,7 @@ import com.elysian.client.util.wrapper.*;
 import net.minecraft.entity.player.*;
 import net.minecraft.entity.*;
 import net.minecraft.potion.Potion;
+import net.minecraft.util.MovementInput;
 import net.minecraft.util.math.*;
 
 import java.util.Objects;
@@ -21,7 +22,57 @@ public class EntityUtil implements Minecraftable
     public static boolean isValidEntity(final Entity player, final float range) {
         return !isntValidEntity(player, range);
     }
-    
+
+    public static double getEntitySpeed(Entity entity) {
+        if (entity != null) {
+            double distTraveledLastTickX = entity.posX - entity.prevPosX;
+            double distTraveledLastTickZ = entity.posZ - entity.prevPosZ;
+            double speed = MathHelper.sqrt((double)(distTraveledLastTickX * distTraveledLastTickX + distTraveledLastTickZ * distTraveledLastTickZ));
+            return speed * 20.0;
+        }
+        return 0.0;
+    }
+
+    public static void moveEntityStrafe(double speed, Entity entity) {
+        if (entity != null) {
+            MovementInput movementInput = EntityUtil.mc.player.movementInput;
+            double forward = movementInput.moveForward;
+            double strafe = movementInput.moveStrafe;
+            float yaw = EntityUtil.mc.player.rotationYaw;
+            if (forward == 0.0 && strafe == 0.0) {
+                entity.motionX = 0.0;
+                entity.motionZ = 0.0;
+            } else {
+                if (forward != 0.0) {
+                    if (strafe > 0.0) {
+                        yaw += (float)(forward > 0.0 ? -45 : 45);
+                    } else if (strafe < 0.0) {
+                        yaw += (float)(forward > 0.0 ? 45 : -45);
+                    }
+                    strafe = 0.0;
+                    if (forward > 0.0) {
+                        forward = 1.0;
+                    } else if (forward < 0.0) {
+                        forward = -1.0;
+                    }
+                }
+                entity.motionX = forward * speed * Math.cos(Math.toRadians(yaw + 90.0f)) + strafe * speed * Math.sin(Math.toRadians(yaw + 90.0f));
+                entity.motionZ = forward * speed * Math.sin(Math.toRadians(yaw + 90.0f)) - strafe * speed * Math.cos(Math.toRadians(yaw + 90.0f));
+            }
+        }
+    }
+
+
+    public static boolean isEntityMoving(Entity entity) {
+        if (entity == null) {
+            return false;
+        }
+        if (entity instanceof EntityPlayer) {
+            return EntityUtil.mc.gameSettings.keyBindForward.isKeyDown() || EntityUtil.mc.gameSettings.keyBindBack.isKeyDown() || EntityUtil.mc.gameSettings.keyBindLeft.isKeyDown() || EntityUtil.mc.gameSettings.keyBindRight.isKeyDown();
+        }
+        return entity.motionX != 0.0 || entity.motionY != 0.0 || entity.motionZ != 0.0;
+    }
+
     public static boolean isntValidEntity(final Entity player, final float range) {
         return player == EntityUtil.mc.player || player.isDead || ((EntityLivingBase)player).getHealth() <= 0.0f || EntityUtil.mc.player.getDistance(player) > range;
     }
