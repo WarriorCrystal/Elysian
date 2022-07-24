3d94ae79d6b0c3d486dafed9c92f6ff7623a47cb
Venuz !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! <v3nuz123@gmail.com>
2022-07-24 18:00:19 -0500

    autorepkly

--- src/main/java/com/elysian/client/Elysian.java
+++ src/main/java/com/elysian/client/Elysian.java
@@ -21,7 +21,7 @@ import java.io.File;
 public final class Elysian {
     public static final String MODID = "elysian";
     public static final String NAME = "Elysian";
-    public static final String VERSION = "0.1.4";
+    public static final String VERSION = "0.1.6";
     public static final String ELYSIAN = NAME + " " + VERSION;
     public static final Minecraft mc = Minecraft.getMinecraft();
     public static EventBus EVENT_BUS = MinecraftForge.EVENT_BUS;
---
+++ src/main/java/com/elysian/client/module/modules/misc/AutoReply.java
@@ -0,0 +1,21 @@
+package com.elysian.client.module.modules.misc;
+
+import com.elysian.client.module.ModuleType;
+import com.elysian.client.module.ToggleableModule;
+import me.zero.alpine.fork.listener.EventHandler;
+import me.zero.alpine.fork.listener.Listener;
+import net.minecraftforge.client.event.ClientChatReceivedEvent;
+
+public class AutoReply extends ToggleableModule {
+
+    public AutoReply() {
+        super("AutoReply", new String[] {"AutoReply"}, "AutoReply", ModuleType.MISC);
+        this.offerProperties(this.keybind);
+    }
+    @EventHandler
+    private Listener<ClientChatReceivedEvent> listener = new Listener<>(event ->{
+        if( event.getMessage().getUnformattedText().contains("whispers: ") && !event.getMessage().getUnformattedText().startsWith(mc.player.getName())){
+            mc.player.sendChatMessage("/r Este es el AutoReply de Elysian, no me hablei qliao");
+        }
+    });
+}
